6fd9a946d4620489616c15367f09b4c2e76f8f54
Evgeny Sidorov <ev.sdrv@ya.ru>
2020-04-08 19:05:24 +0500

    add check login correct

--- server.py
+++ server.py
@@ -18,10 +18,17 @@ class ServerProtocol (asyncio.Protocol):
            self.send_message(decoded)
         else:
             if decoded.startswith("login:"):
-                self.login = decoded.replace("login:", "").replace ("\r\n", "")
-                self.transport.write(f"Hi, {self.login}!\n".encode())
+            #    self.login = decoded.replace("login:", "").replace ("\r\n", "")
+            #    self.transport.write(f"Hi, {self.login}!\n".encode())
+            newlogin = decoded.replace("login:", "").replace("\r\n", "")
+                if self.login_is_correct(newlogin):
+                    self.login = newlogin
+                    self.transport.write(f"Привет, {self.login}!\n".encode())
+                    self.send_history()
+            
             else:
                 self.transport.write("Incorrect login\n".encode())
+                self.transport.close()
 
     def connection_made(self, transport: transports.Transport):
         self.server.clients.append(self)
@@ -38,11 +45,31 @@ class ServerProtocol (asyncio.Protocol):
         for user in self.server.clients:
             user.transport.write(message.encode())
 
+        self.server.messages.append(message)
+        self.server.messages = self.server.messages[-10:]
+
+    def send_history(self):
+
+        if len(self.server.messages) > 0:
+            self.transport.write("Last chat message:\n".encode())
+            for message in self.server.messages:
+                self.transport.write(message.encode())
+
+    def login_is_correct(self, login):
+
+        for user in self.server.clients:
+            if user.login == login:
+                return False
+
+        return True
+
 class Server:
     clients: list
+    message: list
 
     def __init__(self):
         self.clients = []
+        self.message = []
 
     def build_protocol(self):
         return ServerProtocol(self)
